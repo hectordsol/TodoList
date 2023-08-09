d61a1ad916264e78b8a2e825a940950374c0cc5b
Hector Dario SOL <hectordsol@gmail.com>
2023-08-09 07:59:40 -0300

    listen back init

--- back/server.js
+++ back/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 require('dotenv').config();
 const route=require('./routes/todoroutes')
 
 const app = express();
+// app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 4000;
 mongoose
 .connect(process.env.MONGO_DB)
 .then(()=>console.log('connected to MongoDB..'))
 .catch((error)=>console.log(error));
-
-app.route(route); 
+app.use(route); 
 
 app.listen(PORT, ()=> console.log(`Listening on ${PORT}`));
