7db7d391c9b1a0ae903e058510c833060092ccdb
Hector Dario SOL <hectordsol@gmail.com>
2023-08-10 00:17:01 -0300

    CRUD ToDo

---
+++ back/controllers/todoController.js
@@ -0,0 +1,50 @@
+const ToDoModel = require('../models/TodoModel');
+
+module.exports.getTodos = async (req, res) => {
+    const toDos = await ToDoModel.find()
+    res.send(toDos);
+
+}
+module.exports.saveTodo = async (req, res) => {
+    const { text } = req.body;
+    ToDoModel
+    .create({text})
+    .then((todo) =>{
+        console.log("ToDo created Sussefully..");
+        console.log(todo);
+        res.send(todo);
+    })
+    .catch((error)=>
+    {
+        console.error(error);
+    });
+}
+module.exports.updateTodo = async (req, res) => {
+    const { text } = req.body;
+    const { id } = req.params;
+    ToDoModel
+    .findByIdAndUpdate(id,{text})
+    .then((todo) =>{
+        console.log("ToDo updated Sussefully..");
+        console.log(todo);
+        res.send(todo);
+    })
+    .catch((error)=>
+    {
+        console.error(error);
+    });
+}
+module.exports.deleteTodo = async (req, res) => {
+    const { id } = req.params;
+    ToDoModel
+    .findByIdAndDelete(id)
+    .then((todo) =>{
+        console.log("ToDo deleted Sussefully.. id: ",id);
+        console.log(todo);
+        res.send(todo);
+    })
+    .catch((error)=>
+    {
+        console.error(error);
+    });
+}
--- back/routes/todoroutes.js
+++ back/routes/todoroutes.js
@@ -1,7 +1,9 @@
 const {Router}=require('express');
+const { getTodos, saveTodo, deleteTodo, updateTodo } = require('../controllers/todoController');
 const route= Router();
 
-route.get('/', (req, res)=>{
-    res.json({message: 'Hi there..'});
-});
+route.get('/', getTodos);
+route.delete('/:id', deleteTodo);
+route.put('/:id', updateTodo);
+route.post('/', saveTodo);
 module.exports =route;
