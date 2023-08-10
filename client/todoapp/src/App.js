14e9c9a97c27a88389db9eba3d3a856b2bfce1bd
Hector Dario SOL <hectordsol@gmail.com>
2023-08-10 08:55:18 -0300

    create react app

---
+++ client/todoapp/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>
+          App ToDo
+        </h1>
+          <div className="top"></div>  
+      </div>
+    </div>
+  );
+}
+
+export default App;
