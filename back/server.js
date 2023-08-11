00dc4842986bd73f54704f96e1896168adc117b3
Hector Dario SOL <hectordsol@gmail.com>
2023-08-11 10:00:16 -0300

    use cors()

--- back/server.js
+++ back/server.js
@@ -8,7 +8,7 @@ const app = express();
 // app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('cors');
+app.use(cors());
 
 app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Origin', '*');//CORS); // update to match the domain you will make the request from
